942e7a1ea382378c576cac110f3660aa1669731b
Jessica Catama <jessica.catamac@uniagustiniana.edu.co>
2025-08-11 21:13:33 -0500

    Add files via upload

---
+++ Ejercicios de Operadores PyCharm/Suma 3 números.py
@@ -0,0 +1,6 @@
+a=int(input('Ingrese el primer número: '))
+b=int(input('Ingrese el segundo número: '))
+c=int(input('Ingrese el tercer número: '))
+suma = a + b + c
+promedio = suma / 3
+print(f'{promedio}')
